6cacb84380a72ab54bc4610d3f9d90a98341e6ee
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-05 15:25:32 +0100

    use object as storage

--- src/inMemoryCardRepository.js
+++ src/inMemoryCardRepository.js
@@ -1,15 +1,15 @@
 module.exports = function cardRepositoryFactory(recreateFrom) {
-    const storage = new Map();
+    const storage = {};
 
     return {
         async save(card) {
-            const oldEvents = storage.get(card.uuid()) || [];
+            const oldEvents = storage[card.uuid()] || [];
             const allEvents = [...oldEvents, ...card.pendingEvents()];
             card.flushEvents();
-            storage.set(card.uuid(), allEvents);
+            storage[card.uuid()] = allEvents;
         },
         async load(uuid) {
-            return recreateFrom(uuid, storage.get(uuid) || []);
+            return recreateFrom(uuid, storage[uuid] || []);
         }
     };
 };
