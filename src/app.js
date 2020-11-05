d080f8f3ade0ae7db9fb29e4356b6de1209e849d
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-05 16:32:54 +0100

    removing boilerplate

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const now  = function() { return new Date(); };
 const {card, recreateFrom} = require('./card')(now);
 const ClientError = require('./clientError');
 
+// error handling boilerplate
+
+function withErrorHandling(fn) {
+    return async function(req, res) {
+        try {
+            await fn(req.body);
+            res.status(204).send();
+        } catch (e) {
+            if (e instanceof ClientError) {
+                return res.status(400).json({error: e.message});
+            }
+            console.log(e);
+            res.status(500).send();
+        }
+    };
+}
+
 module.exports = function(es) {
     const app = express();
 
@@ -11,74 +28,27 @@ module.exports = function(es) {
 
     app.use(express.json());
 
-    app.post('/limit', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.assignLimit(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
-    app.post('/withdrawal', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.withdraw(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
-    app.post('/repayment', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.repay(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
+    app.post('/limit', withErrorHandling(async (body) => {
+        const c = await repository.load(body.uuid);
+        c.assignLimit(body.amount);
+        await repository.save(c);
+    }));
+    app.post('/withdrawal', withErrorHandling(async (body) => {
+        const c = await repository.load(body.uuid);
+        c.withdraw(body.amount);
+        await repository.save(c);
+    }));
+    app.post('/repayment', withErrorHandling(async (body) => {
+        const c = await repository.load(body.uuid);
+        c.repay(body.amount);
+        await repository.save(c);
+    }));
     app.get('/limit/:uuid', async function (req, res) {
         const c = await repository.load(req.params.uuid);
         res.json({uuid: c.uuid(), limit: c.availableLimit()});
     });
     app.close = function() {
+
         return es.close();
     }
 
