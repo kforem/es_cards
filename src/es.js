207c7cc6c4f414e2c0b7019eeaf9343fd267aebc
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-05 16:06:37 +0100

    one repo to rule them all

--- src/es.js
+++ src/es.js
@@ -1,17 +1,17 @@
-function initStore() {
-    const es = require('eventstore')({
-        type: 'mongodb',
-        host: 'localhost',
-        port: 27017,
-        dbName: 'card_eventstore_payu',
-        eventsCollectionName: 'events',
-        snapshotsCollectionName: 'snapshots',
-        transactionsCollectionName: 'transactions',
-        timeout: 10000,
-        options: {
-            useNewUrlParser: true
-        }
-    });
+function initStore(config = {
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    dbName: 'card_eventstore_payu',
+    eventsCollectionName: 'events',
+    snapshotsCollectionName: 'snapshots',
+    transactionsCollectionName: 'transactions',
+    timeout: 10000,
+    options: {
+        useNewUrlParser: true
+    }
+}) {
+    const es = require('eventstore')(config);
 
     es.close = function() {
         return es.store.disconnect();
